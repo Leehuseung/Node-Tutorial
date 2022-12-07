54fbc34a509efd52b353e575f4d3aeb81716ea22
Leehuseung <gntmd4261@naver.com>
2022-12-07 17:34:31 +0900

    swagger-jsdoc, swagger-ui-express를 이용한 api-doc 생성

--- express/app.js
+++ express/app.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const port = 3000;
 
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Hello World',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./*.js'], // files containing annotations as above
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
+
+
+/**
+ * @openapi
+ * /user:
+ *   post:
+ *     description: 사용자 생성
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
+app.post('/user', (req, res) => {
+    res.send('Hello World!')
+});
