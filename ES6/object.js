1b15bbdff8d65eddf7b9542f721c4d5f5dfa6db9
Leehuseung <gntmd4261@naver.com>
2022-12-11 02:32:22 +0900

    ES6 클래스 예제

--- ES6/object.js
+++ ES6/object.js
@@ -33,3 +33,74 @@ let person = {
     sayHello() { console.log('hello'); }
 }
 
+
+/**
+ * class
+ */
+class Circle {
+    constructor(center,radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    //prototype 메서드
+    area() {
+        return Math.PI*this.radius*this.radius;
+    }
+}
+let c = new Circle({x:0, y:0},2);
+console.log(c.area());
+
+
+/**
+ * 접근자
+ * get,set 키워드를 활용해 게터와 세터를 만들수 있다.
+ *
+ * static을 이용해 정적 메서드를 작성할 수 있다.
+ */
+class Person {
+    constructor(name) {
+        this.name = name;
+        Person.personCount++;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+    static count() {
+        return Person.personCount;
+    }
+    toString(){
+        return 'circle';
+    }
+}
+Person.personCount = 0;
+
+
+/**
+ * extends를 이용한 상속
+ *
+ * super를 이용한 슈퍼타입 메서드 호출
+ */
+class Ball extends Circle {
+    move(dx,dy) {
+        this.center.x += dx;
+        this.center.y += dy;
+    }
+    toString(){
+        let str = super.toString();
+        return str.replace('cicle','ball');
+    }
+}
+let ball = new Ball({x:0, y:0},2);
+console.log(ball.toString());
+
+
+
+
+
+
